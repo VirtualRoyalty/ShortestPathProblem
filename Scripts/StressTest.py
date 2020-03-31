f6ce58492caa58fe3c17a0801ed0bc2bc8291f5d
iamilyasedunov <iliasedunov@yandex.ru>
2020-03-31 20:01:59 +0300

    draw god function implemented, version with final graphics

--- Scripts/StressTest.py
+++ Scripts/StressTest.py
@@ -62,19 +62,21 @@ class StressTest:
         return {'error_graphs' : self.error_graphs, 'seidel' : np.mean(self.working_time['seidel']), 'floyd'  : np.mean(self.working_time['floyd'])}
         
     def run_johnsons_floyd_test(self, number_of_iterations, max_nodes, max_edges, heuristic=None):
-        start_time = time.now()
         bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
         for i in range(number_of_iterations):
             bar.update(i)
             generator = gg.GraphGenerator(max_nodes, max_edges)
-            self.G = generator.generate_weighted_graph(w_range=(1,1), verbose = self.verbose)
+            if heuristic:
+                self.G = generator.generate_weighted_graph(w_range=(1,1), verbose = self.verbose)
+            else:
+                self.G = generator.generate_pseudo_real_graph(w_range=(1,1), verbose = self.verbose)
 
             johnson_algorithm = jsp.JohnsonSP(self.G)
             floyd_algorithm = fsp.FloydSP(self.G)
 
             start_time = time.now()
             pred, floyd_dist = floyd_algorithm.floyd_predecessor_and_distance()
-            # self.working_time['floyd'].append(time.now() - start_time)
+            self.working_time['floyd'].append(time.now() - start_time)
 
             floyd_shortest_paths = floyd_algorithm.get_path_from_predcessor(pred)
             
@@ -92,19 +94,18 @@ class StressTest:
                         floyd_dist, floyd_shortest_paths, johnsons_shortest_paths):
                     self.error_graphs.append(self.G)
                     self.error_counter += 1
-
-            self.working_time = time.now() - start_time
-
         if self.verbose:
             self.print_statistic()
-        return {'error_graphs' : self.error_graphs, 'johnson' : np.mean(self.working_time['johnson']), 'floyd'  : np.mean(self.working_time['floyd'])}
+        return {'error_graphs' : self.error_graphs, 
+                'johnson' : np.mean(self.working_time['johnson']), 
+                'floyd'  : np.mean(self.working_time['floyd'])}
     
 #    def get_G_coord(self, min_coord=0, max_coord=100):
 #        for i in range(len(self.G)):
 #            self.G.nodes[i]['coord'] = (random.randint(min_coord, max_coord), \
 #                                   random.randint(min_coord, max_coord))
     @staticmethod
-    def get_path_lengths(G, floyd_shortest_path, dist_floyd, johnson_shortest_path, verbose=True):
+    def get_path_lengths(G, floyd_shortest_path, dist_floyd, johnson_shortest_path, verbose=False):
         # go through path lists and accumulate weights of edges
         floyd_path_length, johnson_path_lenght = 0, 0
         for i in range(0, len(floyd_shortest_path) - 1):
@@ -115,15 +116,16 @@ class StressTest:
             print("floyd: {} \njohnson: {}\n len: {} {} \n".format(floyd_shortest_path, johnson_shortest_path, floyd_path_length, johnson_path_lenght, ))
         return round(floyd_path_length, 1), round(johnson_path_lenght, 1)
 
-    def is_equal_path_length(self, floyd_dist, floyd_shortest_paths, johnson_shortest_paths):
+    @staticmethod
+    def is_equal_path_length(G, floyd_dist, floyd_shortest_paths, johnson_shortest_paths):
         # find collision
-        for i in range(len(self.G)):
-            for j in range(len(self.G)):
+        for i in range(len(G)):
+            for j in range(len(G)):
                 if floyd_shortest_paths[i][j] != johnson_shortest_paths[i][j]:
                     # print(floyd_shortest_paths[i][j], johnson_shortest_paths[i][j])
 
                     floyd_path_length, johnson_path_lenght = \
-                    self.get_path_lengths(self.G, floyd_shortest_paths[i][j], floyd_dist, johnson_shortest_paths[i][j])
+                    self.get_path_lengths(G, floyd_shortest_paths[i][j], floyd_dist, johnson_shortest_paths[i][j])
                     # print("lens:", floyd_path_length, johnson_path_lenght)
                     return floyd_path_length == johnson_path_lenght
 
